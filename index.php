6f380f844ce763022d1ce78040459b1bda38e616
Mario Alejandro Benítez Orozco <maalben@gmail.com>
2020-10-22 20:23:03 -0500

    Se agrega tipografia de terminator para el título

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Ejemplo2 html y css
+    </title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+
+    <!-- Ésto es un título -->
+    <h1>Biografia de Don Ramon</h1>
+
+    <img id="donramon1" src="images/don_ramon_y_cleotilde.jpg" width="400" height="250">
+
+</body>
+
+</html>
